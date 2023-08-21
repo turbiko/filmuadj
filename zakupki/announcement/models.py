3d48dd99f793b2e03db0d3bb02680ea9c82f3e42
Andriy Voznyuk <andreyv@ukr.net>
2023-08-21 11:43:38 +0200

    Update models.py

--- zakupki/announcement/models.py
+++ zakupki/announcement/models.py
@@ -5,12 +5,18 @@ from django.db import models
 
 from .filetools import filename_creation
 
+
+def class_folder(instance, filename):
+    create_name = filename_creation(instance, filename)
+    return f"ad/{create_name}"
+
 class Announcement(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField(blank=True, null=True)
-    template_document = models.FileField(upload_to=filename_creation, blank=True, null=True)
+    template_document = models.FileField(upload_to=class_folder, blank=True, null=True)
     start_date = models.DateField(null=True, blank=True)
     end_date = models.DateField(null=True, blank=True)
 
     def __str__(self):
         return f"({self.id}) {self.title} "
+
