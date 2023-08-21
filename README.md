f3f77ac8eb8e18fcc6ff2d00b518208b79fcfbcd
Andriy Voznyuk <andreyv@ukr.net>
2023-08-21 16:32:54 +0200

    wagtail zakupivli

--- README.md
+++ README.md
@@ -6,7 +6,6 @@ Installation procedure
 pip install -r requirements.txt
 python manage.py makemigrations
 python manage.py migrate
-python manage.py createsuperuser  
 python manage.py createsuperuser --email user@mail.tld --username username  
 python manage.py runserver 127.0.0.1:8010
 ```
---
+++ zakupivli/zakupivli/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-gpbkn$as3+5#csh99bu%s9t&zwpcz*%y@bdp82b06%!7yu0fsg"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
